23b138116ebb8d036a8562d3a170734cc59e8e81
root <root@cspc018-desktop.(none)>
2011-06-29 17:33:54 +0100

    Added 2 new searches: Search ID by Person and Search Name by ID along with tests

--- lib/search_biomodel.rb
+++ lib/search_biomodel.rb
@@ -1,3 +1,5 @@
+
+
 require 'rubygems'
 require 'savon'
 
@@ -8,7 +10,7 @@ module SysMODB
   end
 
   module SearchBiomodel
-   
+
     def search_by_ChEBIID(search_string)
       client = create_connection
       response = client.request(:biom, "get_models_id_by_ch_ebi_id") do
@@ -35,6 +37,23 @@ module SysMODB
       return response.to_hash[:get_models_id_by_name_response][:get_models_id_by_name_return][:get_models_id_by_name_return]
     end
 
+   def get_models_id_by_person (search_string)
+      client = create_connection
+      response = client.request(:biom, "get_models_id_by_person") do
+        soap.body = { :personName => search_string, :attributes! => { :personName => { "xsi:type" => "xsd:string" } } }  
+      end
+      return response.to_hash[:get_models_id_by_person_response][:get_models_id_by_person_return][:get_models_id_by_person_return]
+   end
+
+   def get_model_name_by_id (search_string)
+      client = create_connection
+      response = client.request(:biom, "get_model_name_by_id") do
+        soap.body = { :id => search_string, :attributes! => { :id => { "xsi:type" => "xsd:string" } } }  
+      end
+      return response.to_hash[:get_model_name_by_id_response][:get_model_name_by_id_return]
+   end
+  
+
 
     private
    
@@ -45,9 +64,9 @@ module SysMODB
         client = Savon::Client.new do
           wsdl.document = "http://www.ebi.ac.uk/biomodels-main/services/BioModelsWebServices?wsdl"
           wsdl.namespace = "http://biomodels.ebi.ac.uk"
-        end #client
-      end #configure
-    end #create_connection
+        end
+      end
+    end 
 
 
   end #SearchBiomodel
--- test/test_search_biomodel.rb
+++ test/test_search_biomodel.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require 'search_biomodel'
+require '/home/cspc018/Work/Repos/gems/search_biomodel/lib/search_biomodel.rb'
 
 
 class TestSearchBiomodel < Test::Unit::TestCase
@@ -7,30 +7,54 @@ class TestSearchBiomodel < Test::Unit::TestCase
   include SysMODB::SearchBiomodel
 
    def test_find_all_models
+      puts "\n\n ---------------------find all models---------------------\n\n"
       response = []
       i=0
       response = get_all_models
-      response.each {|x| puts "\n\n Response #{i+=1}:   #{x[:value].to_s} "}
-      response.each {|x| assert_not_nil x[:value] }
+      response.each {|x| puts "Response #{i+=1}:   #{x[:key].to_s} "}
+      response.each {|x| assert_not_nil x[:key] }
+      assert_not_nil response
    end
 
 
    def test_search_by_ChEBIID
+      puts "\n\n ---------------------search by chebiid---------------------\n\n"
       response = []
       i = 0 
       response = search_by_ChEBIID("CHEBI:15422")
-      response.each {|x| puts "\n\n Response #{i+=1}:   #{x} "}
+      response.each {|x| puts "Response #{i+=1}:   #{x} "}
       response.each {|x| assert_not_nil x }
+      assert_not_nil response
    end
 
-
    def test_search_by_name
+      puts "\n\n ---------------------model ID by name---------------------\n\n"
       response = []
       i = 0 
       response = search_by_name("Kolomeisky2003")    #_MyosinV_Processivity")
-      response.each {|x| puts "\n\n Response #{i+=1}:   #{x} "}
+      response.each {|x| puts "Response #{i+=1}:   #{x} "}
       response.each {|x| assert_not_nil x }
+      assert_not_nil response
    end
+ 
 
+  def test_get_model_name_by_id
+      puts "\n\n ---------------------model name by ID---------------------\n\n"
+      response = get_model_name_by_id("BIOMD0000000190")
+      puts response
+      assert_not_nil response
+  end
+
+
+
+   def test_get_models_id_by_person
+      puts "\n\n ---------------------model ID by person---------------------\n\n"
+      response = []
+      i=0
+      response = get_models_id_by_person("Jim")
+      response.each {|x| puts "Response #{i+=1}:    #{x} "} 
+      response.each {|x| assert_not_nil x }
+      assert_not_nil response
+  end
 
 end
